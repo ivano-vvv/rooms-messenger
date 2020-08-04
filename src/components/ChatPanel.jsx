6fc3d3fdf422b0fa68c605e66689ccdb643f09b6
ivano-vvv <ivanovlad2010@gmail.com>
2020-08-04 09:59:41 +0300

    minor fix — roomname changes dynamically

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -3,12 +3,15 @@ import "./ChatPanel.css";
 import PanelHeader from "./PanelHeader";
 import ChatInputContainer from "./ChatInputContainer";
 import ChatHistoryContainer from "./ChatHistoryContainer";
+import { useSelector } from "react-redux";
 
 export default function ChatPanel(props) {
+  const roomname = useSelector((state) => state.room.name);
+
   return (
     <div className={"chat-panel " + props.className}>
       <PanelHeader containerClass="chat-panel__container">
-        Room Name
+        {roomname || "Room"}
       </PanelHeader>
       <ChatHistoryContainer className="chat-panel__container" />
       <ChatInputContainer
